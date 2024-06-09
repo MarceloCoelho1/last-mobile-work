e2ec7feb933438a84ba6182692904121f1f37709
PedroBenedito1512 <pedro.00000844519@unicap.br>
2024-06-09 05:07:35 -0300

    logica do jogo funcional, mas rudimentar e sem modularização

--- app/src/main/java/com/example/mobilework/screens/PlayerPlayer.kt
+++ app/src/main/java/com/example/mobilework/screens/PlayerPlayer.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
@@ -23,48 +24,100 @@ fun PlayerPlayer(
 ) {
     var player1Choice by remember { mutableStateOf<String?>(null) }
     var player2Choice by remember { mutableStateOf<String?>(null) }
-    var winner by remember { mutableStateOf<String?>(null) } // Corrigido
+    var winner by remember { mutableStateOf<String?>(null) }
+    var isVisible1 by remember { mutableStateOf(true) }
+    var isVisible2 by remember { mutableStateOf(false) }
+    var isVisiblebutton1p1 by remember { mutableStateOf(true) }
+    var isVisiblebutton2p1 by remember { mutableStateOf(true) }
+    var isVisiblebutton3p1 by remember { mutableStateOf(true) }
+    var isVisiblebutton1p2 by remember { mutableStateOf(true) }
+    var isVisiblebutton2p2 by remember { mutableStateOf(true) }
+    var isVisiblebutton3p2 by remember { mutableStateOf(true) }
 
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-        // Área do jogador 1
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(5f)
-                .background(Color.Blue)
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Text(
-                text = player1Name ?: "Jogador 1",
-                fontSize = 50.sp,
-                color = Color.White
-            )
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
+// Área do jogador 1
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(5f)
+                    .background(Color.Blue)
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
             ) {
-                GameButton(
-                    image = painterResource(id = R.drawable.rock),
-                    text = "Rock",
-                    onClick = { player1Choice = "rock"; checkResult(player1Choice, player2Choice, player1Name, player2Name, setWinner = { winner = it }) }
+                Text(
+                    text = player1Name ?: "Jogador 1",
+                    fontSize = 50.sp,
+                    color = Color.White
                 )
-                GameButton(
-                    image = painterResource(id = R.drawable.paper),
-                    text = "Paper",
-                    onClick = { player1Choice = "paper"; checkResult(player1Choice, player2Choice, player1Name, player2Name, setWinner = { winner = it }) }
-                )
-                GameButton(
-                    image = painterResource(id = R.drawable.scissor),
-                    text = "Scissor",
-                    onClick = { player1Choice = "scissor"; checkResult(player1Choice, player2Choice, player1Name, player2Name, setWinner = { winner = it }) }
-                )
-            }
-        }
+                if (isVisible1) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        if (isVisiblebutton1p1) {
+                            GameButton(
+                                image = painterResource(id = R.drawable.rock),
+                                text = "Rock",
+                                onClick = {
+                                    player1Choice = "rock"
+                                    isVisible1 = false
+                                    isVisible2 = true
+                                    isVisiblebutton3p1 = false
+                                    isVisiblebutton2p1 = false
+                                    checkResult(
+                                        player1Choice,
+                                        player2Choice,
+                                        player1Name,
+                                        player2Name,
+                                        setWinner = { winner = it })
+                                }
+                            )
+                        }
+                        if (isVisiblebutton2p1) {
+                            GameButton(
+                                image = painterResource(id = R.drawable.paper),
+                                text = "Paper",
+                                onClick = {
+                                    player1Choice = "paper"
+                                    isVisible1 = false
+                                    isVisible2 = true
+                                    isVisiblebutton3p1 = false
+                                    isVisiblebutton1p1 = false
+                                    checkResult(
+                                        player1Choice,
+                                        player2Choice,
+                                        player1Name,
+                                        player2Name,
+                                        setWinner = { winner = it })
+                                }
+                            )
+                        }
+                        if (isVisiblebutton3p1) {
+                            GameButton(
+                                image = painterResource(id = R.drawable.scissor),
+                                text = "Scissor",
+                                onClick = {
+                                    player1Choice = "scissor"
+                                    isVisible1 = false
+                                    isVisible2 = true
+                                    isVisiblebutton2p1 = false
+                                    isVisiblebutton1p1 = false
+                                    checkResult(
+                                        player1Choice,
+                                        player2Choice,
+                                        player1Name,
+                                        player2Name,
+                                        setWinner = { winner = it })
+                                }
+                            )
+                        }
+                    }
 
+                }
+            }
         // Área de controle no meio
         Box(
             modifier = Modifier
@@ -90,50 +143,98 @@ fun PlayerPlayer(
                 Button(onClick = {
                     player1Choice = null
                     player2Choice = null
-                    winner = null // Corrigido
+                    winner = null
+                    isVisible1 = true
+                    isVisible2 = false
+                    isVisiblebutton1p1 = true
+                    isVisiblebutton2p1 = true
+                    isVisiblebutton3p1 = true
+                    isVisiblebutton1p2 = true
+                    isVisiblebutton2p2 = true
+                    isVisiblebutton3p2 = true
                 }) {
                     Text("Restart")
                 }
             }
         }
 
-        // Área do jogador 2
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(5f)
-                .background(Color.Red)
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Text(
-                text = player2Name ?: "Jogador 2",
-                fontSize = 50.sp,
-                color = Color.White
-            )
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
+// Área do jogador 2
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(5f)
+                    .background(Color.Red)
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
             ) {
-                GameButton(
-                    image = painterResource(id = R.drawable.rock),
-                    text = "Rock",
-                    onClick = { player2Choice = "rock"; checkResult(player1Choice, player2Choice, player1Name, player2Name, setWinner = { winner = it }) }
+                Text(
+                    text = player2Name ?: "Jogador 2",
+                    fontSize = 50.sp,
+                    color = Color.White
                 )
-                GameButton(
-                    image = painterResource(id = R.drawable.paper),
-                    text = "Paper",
-                    onClick = { player2Choice = "paper"; checkResult(player1Choice, player2Choice, player1Name, player2Name, setWinner = { winner = it }) }
-                )
-                GameButton(
-                    image = painterResource(id = R.drawable.scissor),
-                    text = "Scissor",
-                    onClick = { player2Choice = "scissor"; checkResult(player1Choice, player2Choice, player1Name, player2Name, setWinner = { winner = it }) }
-                )
+                if (isVisible2) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        if (isVisiblebutton1p2) {
+                            GameButton(
+                                image = painterResource(id = R.drawable.rock),
+                                text = "Rock",
+                                onClick = {
+                                    player2Choice = "rock"
+                                    isVisible1 = true
+                                    isVisiblebutton3p2 = false
+                                    isVisiblebutton2p2 = false
+                                    checkResult(
+                                        player1Choice,
+                                        player2Choice,
+                                        player1Name,
+                                        player2Name,
+                                        setWinner = { winner = it })
+                                }
+                            )
+                        }
+                        if (isVisiblebutton2p2) {
+                            GameButton(
+                                image = painterResource(id = R.drawable.paper),
+                                text = "Paper",
+                                onClick = {
+                                    player2Choice = "paper"
+                                    isVisible1 = true
+                                    isVisiblebutton3p2 = false
+                                    isVisiblebutton1p2 = false
+                                    checkResult(
+                                        player1Choice,
+                                        player2Choice,
+                                        player1Name,
+                                        player2Name,
+                                        setWinner = { winner = it })
+                                }
+                            )
+                        }
+                        if (isVisiblebutton3p2) {
+                            GameButton(
+                                image = painterResource(id = R.drawable.scissor),
+                                text = "Scissor",
+                                onClick = {
+                                    player2Choice = "scissor"
+                                    isVisible1 = true
+                                    isVisiblebutton2p2 = false
+                                    isVisiblebutton1p2 = false
+                                    checkResult(
+                                        player1Choice,
+                                        player2Choice,
+                                        player1Name,
+                                        player2Name,
+                                        setWinner = { winner = it })
+                                }
+                            )
+                        }
+                    }
+                }
             }
-        }
-
         // Resultado
         winner?.let {
             Box(
@@ -144,8 +245,9 @@ fun PlayerPlayer(
             ) {
                 Text(
                     text = it,
+                    fontWeight = FontWeight.Bold, // ou FontWeight.Normal, FontWeight.Light, etc.
                     fontSize = 50.sp,
-                    color = Color.Black
+                    color = Color.White
                 )
             }
         }
